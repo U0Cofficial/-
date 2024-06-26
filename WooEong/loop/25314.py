a66b72fb6f1cfcda2509cb287ee4fbf675966454
iamyeongg <syz101@soongsil.ac.kr>
2024-06-26 17:42:30 +0900

    [feat] 백준 loop 25314번

---
+++ WooEong/loop/25314.py
@@ -0,0 +1,18 @@
+import sys
+input= sys.stdin.readline
+
+byte = int(input())
+loof= int(byte//4)
+
+result=""
+
+for i in range(loof):
+    result += str("long ")
+    
+
+print(result+"int")
+
+
+
+#a/b -> 실수형 나누기
+#a//b -> 정수형 나누기
