71b04061f3e473aaeb3249f47ddacd7159d3a5ee
iamyeongg <syz101@soongsil.ac.kr>
2024-07-03 15:55:43 +0900

    [feat] 백준 string 10809번

--- WooEong/string/10809.py
+++ WooEong/string/10809.py
@@ -1,12 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-ele = int(input())
-cycle = int(input())
+ele = input().strip()
 
-for i in range (ele):
-    for char in (cycle):
-        
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+result = []
+
+for letter in (alphabet):
+    if letter in ele:
+        result.append(ele.index(letter))
+    else:
+        result.append(-1)
 
 
+print(' '.join(map(str,result)))
 
+#range는 정수 범위를 생성하기 위한 함수
